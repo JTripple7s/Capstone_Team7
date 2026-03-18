84469a34eb9c13b784e7c0288ac795672b6093bb
Seris29 <sergior1929@gmail.com>
2026-03-18 12:44:20 -0700

    changed path

--- app/launch/live_detect.py
+++ app/launch/live_detect.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 import cv2
 
 # Load your trained model
-model = YOLO("runs/detect/train/weights/best.pt")
+model = YOLO("models/best.pt")
 
 # Open webcam (0 = default Mac camera)
 cap = cv2.VideoCapture(0)
